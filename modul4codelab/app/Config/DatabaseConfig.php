750c1a3df3c694fc6a0e53a48f7b82567e526585
Andika Nur Islamy <dikaislamy@gmail.com>
2024-11-22 11:18:27 +0700

    add codelab

---
+++ modul4codelab/app/Config/DatabaseConfig.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace app\Config;
+
+class DatabaseConfig
+{
+    // Database connection settings
+    public $host         = "localhost";
+    public $user         = "root";
+    public $password     = "";
+    public $databaseName = "modul4z_web";
+    public $port         = 3306;
+}
